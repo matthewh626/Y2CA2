85fdd68f2261c8ee7a66aa5e3086eb842f736054
sunbro626 <matthew.h626@gmail.com>
2025-03-31 08:33:40 +0100

    Added waypoint handling methods to the method outllines for the map class

--- Map.java
+++ Map.java
@@ -1,9 +1,7 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
