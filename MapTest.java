ccfdf82568e7958eb737bc6b7e6f80225b9f03b7
sunbro626 <matthew.h626@gmail.com>
2025-05-18 19:40:38 +0100

    Updated Junit test benchmark methods
    changed to use system.nanoTime based benchmarking.
    turns out most of the previous benchmark time was junit overhead,
    DFS and BFS, clock in at an eye watering 4400hz and 1100hz respectivly with this more direct measurement

--- MapTest.java
+++ MapTest.java
@@ -1,10 +1,11 @@
 package com.ca2.routefinder;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.ca2.routefinder.Map.Path;
+
+import javafx.scene.image.WritableImage;
 
 @SuppressWarnings("unused")
 class MapTest {
@@ -41,7 +42,7 @@ static Map testInstance;
 		try {
 			Path temp = testInstance.findBFSPath(testInstance.getStop("Neulaa"), testInstance.getStop("Zieglergasse"));
 			assert(temp != null);
-			//System.out.println(temp.toString());
+			System.out.println(temp.toString());
 		} catch (DestionationUnreachableException e) {
 			e.printStackTrace();
 		}
@@ -51,9 +52,56 @@ static Map testInstance;
 	void bfsBenchTest() {
 		try {
 			Path temp = null;
-			for (int i = 0; i <100; i++) {
-			temp = testInstance.findBFSPath(testInstance.getStop("Neulaa"), testInstance.getStop("Zieglergasse"));
+			int minMs = 1000;
+			int maxMs = 0;
+			int avgMs = 0;
+			int lastMs = 0;
+			System.out.println("Starting Breadth first search benchmark...");
+			for (int i = 0; i < 101; i++) {
+			long startTime = System.nanoTime();
+			testInstance.findBFSPath(testInstance.getStop("Neulaa"), testInstance.getStop("Zieglergasse"));
+			long endTime = System.nanoTime();
+			lastMs = (int) (endTime - startTime);
+			//System.out.println("Breadth First Search took " + lastMs + " nanoseconds");
+			minMs = lastMs < minMs ? lastMs : minMs;
+			maxMs = lastMs > maxMs ? lastMs : maxMs;
+			avgMs = (avgMs + lastMs)/2;
 			}
+			System.out.println("Benchmark compleate, Stats;");
+			System.out.println("Minimum time: " + minMs + "ns, equivelent to " + 1000000000/minMs + " FPS");
+			System.out.println("Maximum time: " + maxMs + "ns, equivelent to " + 1000000000/maxMs + " FPS");
+			System.out.println("Average time: " + avgMs + "ns, equivelent to " + 1000000000/avgMs + " FPS");
+			temp = testInstance.findBFSPath(testInstance.getStop("Neulaa"), testInstance.getStop("Zieglergasse"));
+			assert(temp != null);
+		} catch (DestionationUnreachableException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void dfsBenchTest() {
+		try {
+			Path temp = null;
+			int minMs = 1000;
+			int maxMs = 0;
+			int avgMs = 0;
+			int lastMs = 0;
+			System.out.println("Starting Depth first search benchmark...");
+			for (int i = 0; i < 101; i++) {
+			long startTime = System.nanoTime();
+			testInstance.findDFSPath(testInstance.getStop("Neulaa"), testInstance.getStop("Zieglergasse"));
+			long endTime = System.nanoTime();
+			lastMs = (int) (endTime - startTime);
+			//System.out.println("Breadth First Search took " + lastMs + " nanoseconds");
+			minMs = lastMs < minMs ? lastMs : minMs;
+			maxMs = lastMs > maxMs ? lastMs : maxMs;
+			avgMs = (avgMs + lastMs)/2;
+			}
+			System.out.println("Benchmark compleate, Stats;");
+			System.out.println("Minimum time: " + minMs + "ns, equivelent to " + 1000000000/minMs + " FPS");
+			System.out.println("Maximum time: " + maxMs + "ns, equivelent to " + 1000000000/maxMs + " FPS");
+			System.out.println("Average time: " + avgMs + "ns, equivelent to " + 1000000000/avgMs + " FPS");
+			temp = testInstance.findDFSPath(testInstance.getStop("Neulaa"), testInstance.getStop("Zieglergasse"));
 			assert(temp != null);
 		} catch (DestionationUnreachableException e) {
 			e.printStackTrace();
